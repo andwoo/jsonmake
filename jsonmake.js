839eca7c62055e8828ded86bca81c062574029c9
Andrew M <andrewlmanley@gmail.com>
2016-03-08 21:28:57 -0500

    -added comments. -removed es6 string templating to be browser compliant.

--- jsonmake.js
+++ jsonmake.js
@@ -1,15 +1,29 @@
+/**
+ * Creates a new instance of JSONMake.
+ * @constructor
+ * @param {Object} cfg - Config object containing keys for overriding the method names for 'readJSON' and 'writeJSON'.
+ */
 function JSONMake(cfg) {
   JSONMake.prototype.config(cfg);
 }
+/**
+ * Overwrites the default config object.
+ * @param {Object} cfg - Config object containing keys for overriding the method names for 'readJSON' and 'writeJSON'. 
+ */
 JSONMake.prototype.config = function(config) {
-  //override config
   var temp = JSONMake.prototype.isUndefined(config) ? {} : config;
   this.config = {
     readJSON: temp.readJSON || "readJSON",
     writeJSON: temp.writeJSON || "writeJSON"
   }
 };
-
+/**
+ * Generates or populates an object with data from the source. If the key does not exist on the 'obj', it will not be added and skipped. 
+ * If the method 'readJSON' exists on the obj, it will only populate those keys.
+ * @param {Object} obj - Function constructor or object to populate with data parsed from JSON.
+ * @param {string} source - JSON string data or an already parsed object.
+ * @return {Object} Object containing parsed data.
+ */
 JSONMake.prototype.make = function(obj, source) {
   //generates a class from json
   var makeObj;
@@ -22,12 +36,12 @@ JSONMake.prototype.make = function(obj, source) {
     makeObj = obj;
   }
   else {
-    console.warn(`Make warning: ${obj.constructor.name} is not a function or object`);
+    console.warn("Make warning: " + obj.constructor.name + " is not a function or object");
     return obj;
   }
   
   if(JSONMake.prototype.isUndefined(source) || source === "" ) {
-    console.warn(`Make warning: JSON source '${source}' is undefined or invalid`);
+    console.warn("Make warning: JSON source " + source + " is undefined or invalid");
     return makeObj;
   }
   //check if jsonstring is a string or already parsed json
@@ -36,7 +50,7 @@ JSONMake.prototype.make = function(obj, source) {
     parsed = JSONMake.prototype.isString(source) ? JSON.parse(source) : source;
   }
   catch(error) {
-    console.warn(`Make warning: JSON source '${source}' is undefined or invalid`);
+    console.warn("Make warning: JSON source " + source + " is undefined or invalid");
     return makeObj;
   }
   
@@ -60,10 +74,15 @@ JSONMake.prototype.make = function(obj, source) {
   }
   return makeObj;
 };
-  
+/**
+ * Convers the obj to a JSON string. If the method 'writeJSON' exists on the object, it will only convert those properties to JSON while 
+ * ignoring the rest.
+ * @param {Object} obj - Object to convert to a JSON string.
+ * @return {string} Stringified JSON object.
+ */
 JSONMake.prototype.dump = function(obj) {
   if (JSONMake.prototype.isUndefined(obj)) {
-    console.warn(`Dump warning: object is undefined`);
+    console.warn("Dump warning: object is undefined");
     return "{}"; //empty json object
   }
   else if(this.config.writeJSON in obj && JSONMake.prototype.isFunction(obj[this.config.writeJSON])) {
@@ -77,30 +96,57 @@ JSONMake.prototype.dump = function(obj) {
       obj = tempObj;
     }
     else {
-      console.warn(`Dump warning: blueprint returned by '${obj.constructor.name}' is not an array`);
+      console.warn("Dump warning: blueprint returned by " + obj.constructor.name + " is not an array");
     }
   }
   
   return JSON.stringify(obj);
 };
-
-//helper methods
+/**
+ * Method checks if the passed in parameter is undefined or null. 
+ * @param {any} thing - Value to test
+ * @return {bool} Returns TRUE if the parameter is undefined or null.
+ */
 JSONMake.prototype.isUndefined = function(thing) {
   return thing === undefined || thing === null;
 }
+/**
+ * Method checks if the passed in parameter is a function. 
+ * @param {any} thing - Value to test
+ * @return {bool} Returns TRUE if the parameter is a function.
+ */
 JSONMake.prototype.isFunction = function(thing) {
   return typeof(thing) === "function";
 }
+/**
+ * Method checks if the passed in parameter is of type Object. 
+ * @param {any} thing - Value to test
+ * @return {bool} Returns TRUE if the parameter is of type Object.
+ */
 JSONMake.prototype.isObject = function(thing) {
   return !JSONMake.prototype.isUndefined(thing) && typeof(thing) === "object" && !JSONMake.prototype.isArray(thing);
 }
+/**
+ * Method checks if the passed in parameter is of type array. 
+ * @param {any} thing - Value to test
+ * @return {bool} Returns TRUE if the parameter is of type array.
+ */
 JSONMake.prototype.isArray = function(thing) {
   return thing instanceof Array;
 }
+/**
+ * Method checks if the passed in parameter is of type string. 
+ * @param {any} thing - Value to test
+ * @return {bool} Returns TRUE if the parameter is of type string.
+ */
 JSONMake.prototype.isString = function(thing) {
   return typeof(thing) === "string";
 }
-
+/**
+ * Returns the property keys of the given object.
+ * @param {Object} obj - Object to retrieve property keys from
+ * @return {string[]} Array of keys.
+ */
 JSONMake.prototype.getObjectKeys = function(obj) {
   if(Object.keys) {
     return Object.keys(obj);
